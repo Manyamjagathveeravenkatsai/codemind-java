675e2d2ed5a299d30250448c775b965ef6b347cc
jagath veera venkata sai manyam <84398703+Manyamjagathveeravenkatsai@users.noreply.github.com>
2022-09-03 15:02:35 +0530

    import java.util.*; class Palindromeinrange { 	public static boolean palindrome(int n) 	{ 		int temp=n; 		int sum=0,r; 		while(n>0) 		{ 			r=n%10; 			sum=sum*10+r; 			n=n/10; 		} 		if (temp==sum) 			return true; 		else 			return false; 	} 	public static void main(String args[]) 	{ 		Scanner sc=new Scanner(System.in); 		int n,m,i; 		m=sc.nextInt(); 		n=sc.nextInt(); 		for(i=m;i<=n;i++) 		{ 			if(palindrome(i)) 			  System.out.print(i+" "); 		} 	} }

---
+++ Program_to_print_all_palindromes_in_a_given_range_.java
@@ -0,0 +1,31 @@
+import java.util.*;
+class Palindromeinrange
+{
+	public static boolean palindrome(int n)
+	{
+		int temp=n;
+		int sum=0,r;
+		while(n>0)
+		{
+			r=n%10;
+			sum=sum*10+r;
+			n=n/10;
+		}
+		if (temp==sum)
+			return true;
+		else 
+			return false;
+	}
+	public static void main(String args[]) 
+	{
+		Scanner sc=new Scanner(System.in);
+		int n,m,i;
+		m=sc.nextInt();
+		n=sc.nextInt();
+		for(i=m;i<=n;i++)
+		{
+			if(palindrome(i))
+			  System.out.print(i+" ");
+		}
+	}
+}
