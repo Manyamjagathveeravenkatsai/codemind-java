a259872a0a077e8916de2b2f0f664f647883d596
jagath veera venkata sai manyam <84398703+Manyamjagathveeravenkatsai@users.noreply.github.com>
2022-09-10 19:03:05 +0530

    import java.util.*; class  CompoundInterest {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         double p,t,r;         double i;         p=sc.nextDouble();         r=sc.nextDouble();         t=sc.nextDouble();         i=p*(Math.pow((1+r/100),t));         System.out.format("%.2f",i);     } }

---
+++ Compound_interest.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class  CompoundInterest
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        double p,t,r;
+        double i;
+        p=sc.nextDouble();
+        r=sc.nextDouble();
+        t=sc.nextDouble();
+        i=p*(Math.pow((1+r/100),t));
+        System.out.format("%.2f",i);
+    }
+}
