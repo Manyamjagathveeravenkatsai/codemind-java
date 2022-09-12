f53358ab5da7651e2262bf9f79e30f7b7aca8d40
jagath veera venkata sai manyam <84398703+Manyamjagathveeravenkatsai@users.noreply.github.com>
2022-09-12 13:58:48 +0530

    import java.util.Scanner; class downtriangle {     public static void main(String args[])     {     	Scanner sc = new Scanner(System.in);     	int r = sc.nextInt();     	for (int i=r;i<=r&&i>0; i--)     	{     		for (int j=1;j<=i;j++)     		{     			System.out.print(j);    			}     		System.out.println();     	}     	sc.close();     } }

--- print_the_following_pattern.java
+++ print_the_following_pattern.java
@@ -1,18 +1,18 @@
-import java.util.*;
-class printalphabets
+import java.util.Scanner;
+class downtriangle
 {
-	public static void main(String args[] ) 
-	{
-		Scanner sc=new Scanner(System.in);
-		int a;
-		a=sc.nextInt();
-		for(int i=1;i<=a;i++)
-		{
-			for (int j=1;j<=i;j++) 
-			{
-				System.out.print(j);
-			}
-			System.out.println();
-		}
-	}
+    public static void main(String args[])
+    {
+    	Scanner sc = new Scanner(System.in); 
+    	int r = sc.nextInt();
+    	for (int i=r;i<=r&&i>0; i--)
+    	{
+    		for (int j=1;j<=i;j++)
+    		{
+    			System.out.print(j);
+   			}
+    		System.out.println();
+    	}
+    	sc.close();
+    }
 }
