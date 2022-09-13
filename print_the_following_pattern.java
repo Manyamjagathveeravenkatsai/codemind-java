5e60d5869da12654a72393935b81e6d3494e41cb
jagath veera venkata sai manyam <84398703+Manyamjagathveeravenkatsai@users.noreply.github.com>
2022-09-13 18:58:46 +0530

    import java.util.*; class jagath { 	static void print(int n) 	{ 		int r1=1,r2=1; 		for (int i=0;i<n;i++) 		{ 			for (int j=n-1;j>i;j--) 			{ 				System.out.print(" "); 			} 			for (int k=1;k<=r1;k++) 			{ 				System.out.print(Math.abs(k-r2)); 			} 			r1+=2; 			r2++; 			System.out.println(); 		} 	} 	public static void main (String args[]) 	{ 		Scanner sc=new Scanner(System.in); 		int n; 		n=sc.nextInt(); 		print(n); 	} }

--- print_the_following_pattern.java
+++ print_the_following_pattern.java
@@ -1,22 +1,29 @@
 import java.util.*;
-class sample
+class jagath
 {
-    public static void main(String args[])
-    {
-        Scanner sc=new Scanner(System.in);
-        int n;
-        n=sc.nextInt();
-        for(int i=1;i<=n;i++)
-        {
-            for(int j=n-i;j>=1;j--)
-            {
-                System.out.print(" ");
-            }
-            for (int j=1;j<=2*i-1;j++)
-            {
-                System.out.print(i);
-            }
-            System.out.println("");
-        }
-    }
+	static void print(int n)
+	{
+		int r1=1,r2=1;
+		for (int i=0;i<n;i++)
+		{
+			for (int j=n-1;j>i;j--)
+			{
+				System.out.print(" ");
+			}
+			for (int k=1;k<=r1;k++)
+			{
+				System.out.print(Math.abs(k-r2));
+			}
+			r1+=2;
+			r2++;
+			System.out.println();
+		}
+	}
+	public static void main (String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		int n;
+		n=sc.nextInt();
+		print(n);
+	}
 }
