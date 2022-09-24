d8c15eb428c6a3eec49e8d6359047b6a35c57ce4
jagath veera venkata sai manyam <84398703+Manyamjagathveeravenkatsai@users.noreply.github.com>
2022-09-24 21:00:38 +0530

    import java.util.*; class CircularPrime {     public static boolean isPrime(int n)     {         if(n==1)         return false;         for(int i=2;i<=(int)Math.sqrt(n);i++)         {             if(n%i==0)             return false;         }         return true;     }     public static int revDigit(int n)     {         int rev=0,temp=n;         while(n!=0)         {             rev=rev*10+n%10;             n=n/10;         }         return rev;     }     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int m;         m=sc.nextInt();             if(isPrime(m)&&isPrime(revDigit(m)))             System.out.println("circular prime");             else if(!isPrime(m))             System.out.println("not prime");             else               System.out.println("prime but not a circular prime");             } }

--- circular_prime.java
+++ circular_prime.java
@@ -1,40 +1,37 @@
 import java.util.*;
-class Circularprime
+class CircularPrime
 {
-	public static boolean isprime(int n)
-	{
-		int count=0;
-		for(int i=2;i<=(int)Math.sqrt(n);i++)
-		{
-			if (n%i==0)
-				count++;
-		}
-		if(count==0)
-			return true;
-		else 
-			return false;
-	}
-	public static int reverse(int p)
-	{
-		int sum=0,r;
-		while(p>0)
-		{
-			r=p%10;
-			sum=sum*10+r;
-			p=p/10;
-		}
-		return sum;
-	}
-	public static void main(String args[]) 
-	{
-		Scanner sc=new Scanner(System.in);
-		int m;
-		m=sc.nextInt();
-		if(!isprime(m))
-			System.out.println("not prime");
-		else if (isprime(reverse(m)))
-			System.out.println("circular prime");
-		else 
-			System.out.println("prime but not a circular prime");
-	}
+    public static boolean isPrime(int n)
+    {
+        if(n==1)
+        return false;
+        for(int i=2;i<=(int)Math.sqrt(n);i++)
+        {
+            if(n%i==0)
+            return false;
+        }
+        return true;
+    }
+    public static int revDigit(int n)
+    {
+        int rev=0,temp=n;
+        while(n!=0)
+        {
+            rev=rev*10+n%10;
+            n=n/10;
+        }
+        return rev;
+    }
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int m;
+        m=sc.nextInt();
+            if(isPrime(m)&&isPrime(revDigit(m)))
+            System.out.println("circular prime");
+            else if(!isPrime(m))
+            System.out.println("not prime");
+            else
+              System.out.println("prime but not a circular prime");
+            }
 }
