c87c3d8b62c0716b1aaebeba7deba54c3f384d6c
jagath veera venkata sai manyam <84398703+Manyamjagathveeravenkatsai@users.noreply.github.com>
2022-09-24 20:52:14 +0530

    import java.util.*; class Sample {     public static boolean isPrime(int n)     {         if (n==1)         return false;         for(int i=2;i<=(int)Math.sqrt(n);i++)         {             if(n%i==0)             return false;         }         return true;     }     public static boolean isPalin(int m)     {         int rev=0,temp=m;         while(m!=0)         {             rev=rev*10+m%10;             m=m/10;         }         if(temp==rev)         return true;         else         return false;     }     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n;         n=sc.nextInt();         for(int i=n+1; ;i++)         {             if(isPrime(i)&&isPalin(i))             {             System.out.println(i);             break;             }         }     } }

--- Program_to_find_out_the_next_prime_palindrome_number.java
+++ Program_to_find_out_the_next_prime_palindrome_number.java
@@ -1,46 +1,42 @@
 import java.util.*;
-class Primepalindrome
+class Sample
 {
-	public static boolean isprime(int n)
-	{
-		int count=0;
-		for(int i=2;i<=(int)Math.sqrt(n);i++)
-		{
-			if (n%i==0)
-				count++;
-		}
-		if(count==0)
-			return true;
-		else 
-			return false;
-	}
-	public static boolean palindrome(int n)
-	{
-		int temp=n;
-		int sum=0,r;
-		while(n>0)
-		{
-			r=n%10;
-			sum=sum*10+r;
-			n=n/10;
-		}
-		if (temp==sum)
-			return true;
-		else 
-			return false;
-	}
-	public static void main(String args[]) 
-	{
-		Scanner sc=new Scanner(System.in);
-		int n,i;
-		n=sc.nextInt();
-		for(i=n+1;;i++)
-		{
-			if (isprime(i) && palindrome(i))
-			{
-				System.out.println(i);
-				break;
-			}
-		}
-	}
+    public static boolean isPrime(int n)
+    {
+        if (n==1)
+        return false;
+        for(int i=2;i<=(int)Math.sqrt(n);i++)
+        {
+            if(n%i==0)
+            return false;
+        }
+        return true;
+    }
+    public static boolean isPalin(int m)
+    {
+        int rev=0,temp=m;
+        while(m!=0)
+        {
+            rev=rev*10+m%10;
+            m=m/10;
+        }
+        if(temp==rev)
+        return true;
+        else
+        return false;
+    }
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n;
+        n=sc.nextInt();
+        for(int i=n+1; ;i++)
+        {
+            if(isPrime(i)&&isPalin(i))
+            {
+            System.out.println(i);
+            break;
+            }
+        }
+    }
 }
