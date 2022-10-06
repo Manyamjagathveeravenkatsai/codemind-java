de7ed5b06ad263c977de344b904070a9bed3e602
jagath veera venkata sai manyam <84398703+Manyamjagathveeravenkatsai@users.noreply.github.com>
2022-10-06 18:34:58 +0530

    import java.util.*; class Abundantnumber {     public static boolean abundant(int n)     {         int sum=0;         for(int i=1;i<=n/2;i++)         {             if(n%i==0)             sum=sum+i;         }         if(sum>n)         return true;         else         return false;     }     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         if(abundant(n))         System.out.println("True");         else         System.out.println("False");         sc.close();     } }

--- Abundant_Numbers.java
+++ Abundant_Numbers.java
@@ -1,19 +1,27 @@
 import java.util.*;
-class AbundantNumbers
+class Abundantnumber
 {
-    public static void main(String args[])
+    public static boolean abundant(int n)
     {
-        Scanner sc=new Scanner(System.in);
-        int n,sum=0;
-        n=sc.nextInt();
+        int sum=0;
         for(int i=1;i<=n/2;i++)
         {
             if(n%i==0)
             sum=sum+i;
         }
         if(sum>n)
+        return true;
+        else
+        return false;
+    }
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        if(abundant(n))
         System.out.println("True");
         else
         System.out.println("False");
+        sc.close();
     }
 }
