929434fca4d9c150cf84cb80c584ffa521de28ae
jagath veera venkata sai manyam <84398703+Manyamjagathveeravenkatsai@users.noreply.github.com>
2022-10-07 15:05:04 +0530

    import java.util.*; class countelementsincirculararray { 	public static void main(String args[]) 	{ 		int x[],n,i,count=0; 		Scanner sc=new Scanner(System.in); 		n=sc.nextInt(); 		x=new int[n]; 		for(i=0;i<n;i++) 			x[i]=sc.nextInt(); 		for(i=1;i<n-1;i++) 			if(x[i-1]%2==0 && x[i+1]%2!=0 || x[i-1]%2!=0 && x[i+1]%2==0) 				count++; 			if(x[n-1]%2==0 && x[1]%2!=0 || x[n-1]%2!=0 && x[1]%2==0) 				count++; 			if(x[n-2]%2==0 && x[0]%2!=0 || x[n-2]%2!=0 && x[0]%2==0) 				count++; 		System.out.println(count); 	} }

---
+++ count_elements_in_an_array_in_circular_form.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class countelementsincirculararray
+{
+	public static void main(String args[]) 
+	{
+		int x[],n,i,count=0;
+		Scanner sc=new Scanner(System.in);
+		n=sc.nextInt();
+		x=new int[n];
+		for(i=0;i<n;i++)
+			x[i]=sc.nextInt();
+		for(i=1;i<n-1;i++)
+			if(x[i-1]%2==0 && x[i+1]%2!=0 || x[i-1]%2!=0 && x[i+1]%2==0)
+				count++;
+			if(x[n-1]%2==0 && x[1]%2!=0 || x[n-1]%2!=0 && x[1]%2==0)
+				count++;
+			if(x[n-2]%2==0 && x[0]%2!=0 || x[n-2]%2!=0 && x[0]%2==0)
+				count++;
+		System.out.println(count);
+	}
+}
