7a347372f7d8ce477604cb0610d34ef166d33068
jagath veera venkata sai manyam <84398703+Manyamjagathveeravenkatsai@users.noreply.github.com>
2022-10-08 14:03:33 +0530

    import java.util.*; class ntimerepeatedelements { 	public static void main(String args[]) 	{ 		Scanner sc=new Scanner(System.in); 		int x[],n,i,j,k,count=0,flag=0; 		n=sc.nextInt(); 		x=new int[n]; 		for (i=0;i<n;i++) 			x[i]=sc.nextInt(); 		k=sc.nextInt(); 		for(i=0;i<n;i++) 		{ 			if(x[i]!=-999) 			{ 				count=1; 				for(j=0;j<n;j++) 				{ 					if(x[i]==x[j]&&i!=j) 					{ 						count++; 						x[j]=-999; 					} 				} 				if(count==k) 				{ 					System.out.print(x[i]+" "); 					flag=1; 				} 			} 		} 		if(flag==0) 			System.out.print("-1"); 	} }

---
+++ N_times_repeated_elements.java
@@ -0,0 +1,36 @@
+import java.util.*;
+class ntimerepeatedelements
+{
+	public static void main(String args[]) 
+	{
+		Scanner sc=new Scanner(System.in);
+		int x[],n,i,j,k,count=0,flag=0;
+		n=sc.nextInt();
+		x=new int[n];
+		for (i=0;i<n;i++)
+			x[i]=sc.nextInt();
+		k=sc.nextInt();
+		for(i=0;i<n;i++)
+		{
+			if(x[i]!=-999)
+			{
+				count=1;
+				for(j=0;j<n;j++)
+				{
+					if(x[i]==x[j]&&i!=j)
+					{
+						count++;
+						x[j]=-999;
+					}
+				}
+				if(count==k)
+				{
+					System.out.print(x[i]+" ");
+					flag=1;
+				}
+			}
+		}
+		if(flag==0)
+			System.out.print("-1");
+	}
+}
