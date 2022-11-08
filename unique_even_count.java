599fe65375c88fffaa38a0360d2146c4e160718d
jagath veera venkata sai manyam <84398703+Manyamjagathveeravenkatsai@users.noreply.github.com>
2022-11-08 06:20:14 +0530

    import java.util.*; class UniqueEvenCount {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int x[],n,i,j,count=0,sum=0;         n=sc.nextInt();         x=new int[n];         for(i=0;i<n;i++)             x[i]=sc.nextInt();         for(i=0;i<n;i++)         {             count=1;             if(x[i]!=-999)             {                 for(j=0;j<n;j++)                 {                     if(x[i]==x[j]&&i!=j)                     {                         count++;                         x[j]=-999;                     }                 }                 if(x[i]%2==0)                     sum++;             }         }         System.out.println(sum);     } }

--- unique_even_count.java
+++ unique_even_count.java
@@ -4,22 +4,22 @@ class UniqueEvenCount
     public static void main(String args[])
     {
         Scanner sc=new Scanner(System.in);
-        int n,x[],i,j,count=0,sum=0;
+        int x[],n,i,j,count=0,sum=0;
         n=sc.nextInt();
         x=new int[n];
         for(i=0;i<n;i++)
-        x[i]=sc.nextInt();
+            x[i]=sc.nextInt();
         for(i=0;i<n;i++)
         {
-           count=1;
-           if(x[i]!=-99)
+            count=1;
+            if(x[i]!=-999)
             {
                 for(j=0;j<n;j++)
                 {
-                    if(x[i]==x[j] && i!=j)
+                    if(x[i]==x[j]&&i!=j)
                     {
                         count++;
-                        x[j]=-99;
+                        x[j]=-999;
                     }
                 }
                 if(x[i]%2==0)
